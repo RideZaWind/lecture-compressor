b7a3200a670a177a4fdd3a22d352eeba0ef5cc90
RideZaWind <114116286+RideZaWind@users.noreply.github.com>
2026-03-23 02:11:06 -0700

    yt-dlp Fix

--- app/routes.py
+++ app/routes.py
@@ -30,7 +30,8 @@ def customize():
         'quiet': True, 
         'skip_download': True,
         'cookiefile': COOKIES_PATH,
-        'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+        'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        'extractor_args': {'youtube': {'player_client': ['android', 'web']}},
     }
 
     try:
